134f8e99497f7f9c30b390b0f10d502c0ca847cc
yoyololicon <ya70201@gmail.com>
2017-11-21 09:38:46 +0800

    initial comment. first just read the data.

---
+++ KNN_classifier.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sys
+import numpy as np
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('usage: %s <train file> <test file>' % sys.argv[0])
+        sys.exit(1)
+    train_data = pd.read_csv(sys.argv[1]).values
+    test_data = pd.read_csv(sys.argv[2]).values
+    [train_index, train_name, train_value, train_class] = np.split(train_data, [1, 2, -1], axis=1)
+    [test_index, test_name, test_value, test_class] = np.split(test_data, [1, 2, -1], axis=1)
+    total_train = np.squeeze(train_index[-1]+1)
+    total_test = np.squeeze(test_index[-1]+1)
+    print total_test, total_train
